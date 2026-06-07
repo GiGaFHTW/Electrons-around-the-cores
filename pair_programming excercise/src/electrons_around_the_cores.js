14ad110a5d5ce4c3ec4869399bc6b05320f0b8a1
if25b180 <if25b180@technikum-wien.at>
2026-06-07 18:58:43 +0200

    production code 2
    Test 2 passed

--- pair_programming excercise/src/electrons_around_the_cores.js
+++ pair_programming excercise/src/electrons_around_the_cores.js
@@ -1,7 +1,16 @@
 // StringCalculator.js
 function Electrons() {}
 Electrons.prototype.add = function(list) {
+	let result = 0;
 	let listLength = list.length;
+	
+	for (let i = 0; i < listLength; i++) {
+		if (list[i] % 2 != 0) {
+			continue;
+		}
+		
+		result += list[i];
+	}
 
-	return list[0] + list[listLength - 1];
+	return result;
 }

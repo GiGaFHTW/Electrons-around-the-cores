6dade9773b444d768d333794d8d633eaaf911f37
GiGaGum <if25b075@technikum-wien.at>
2026-06-07 18:53:40 +0200

    failing test 2

--- pair_programming excercise/spec/electrons_around_the_cores_spec.js
+++ pair_programming excercise/spec/electrons_around_the_cores_spec.js
@@ -7,4 +7,9 @@ describe('Electrons around the Cores', function() {
 		let list = [ 1, 2, 3, 4, 5 ];
 		expect(electrons.add(list)).toEqual(6);
 	});
+
+	it('should return 4 for [ 2, 2, 3, 3 ]', function() {
+		let list = [ 2, 2, 3, 3 ];
+		expect(electrons.add(list)).toEqual(4);
+	});
 });

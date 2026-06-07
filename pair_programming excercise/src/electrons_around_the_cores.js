85ac6e088eeeaec0577b32e244345da6a980c9b9
GiGaGum <if25b075@technikum-wien.at>
2026-06-07 19:07:24 +0200

    production code 3

--- pair_programming excercise/src/electrons_around_the_cores.js
+++ pair_programming excercise/src/electrons_around_the_cores.js
@@ -4,11 +4,11 @@ Electrons.prototype.add = function(list) {
 	let result = 0;
 	
 	for (let i = 0; i < list.length; i++) {
-		if (list[i] % 2 != 0) {
+		if (list[i] % 2 == 0) {
 			continue;
 		}
 		
-		result += list[i];
+		result += list[i] - 1;
 	}
 
 	return result;

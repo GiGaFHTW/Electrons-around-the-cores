4ec28f2733c8ded684ecdc9fac0f4e336d2f095f
if25b180 <if25b180@technikum-wien.at>
2026-06-07 18:36:56 +0200

    failing test 1

--- pair_programming excercise/spec/electrons_around_the_cores_spec.js
+++ pair_programming excercise/spec/electrons_around_the_cores_spec.js
@@ -3,4 +3,8 @@ describe('Electrons around the Cores', function() {
 		electrons = new Electrons();
 	});
 
+	it('should return 6 for [ 1, 2, 3, 4, 5 ]', function() {
+		let list = [ 1, 2, 3, 4, 5 ];
+		expect(electrons.add(list)).toEqual(6);
+	});
 });
